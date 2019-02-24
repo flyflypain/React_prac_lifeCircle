6f7698db5c9f1b6e2ea35b6890aa746c9f03d016
黎晟 <reisei1991@gmail.com>
2019-02-24 15:13:59 +0900

    prototype-1: clear unnecessary codes

--- src/App.js
+++ src/App.js
@@ -3,76 +3,10 @@ import "./App.css";
 import ChildComponent from "./components/childComponent";
 
 class App extends Component {
-  state = {
-    value: ""
-  };
-
-  constructor(props) {
-    super(props);
-    console.log("parent construtor");
-  }
-
-  componentWillMount() {
-    console.log("parent componentWillMount", this.state.value);
-  }
-
-  componentDidMount() {
-    console.log("parent componentDidMount", this.state.value);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("parent componentWillReceiveProps");
-  }
-
-  shouldComponentUpdate() {
-    console.log("parent shouldComponentUpdate", this.state.value);
-    return true; // 记得要返回true
-  }
-
-  componentWillUpdate() {
-    console.log("parent componentWillUpdate", this.state.value);
-  }
-
-  componentDidUpdate() {
-    console.log("parent componentDidUpdate", this.state.value);
-  }
-
-  componentWillUnmount() {
-    console.log("parent componentWillUnmount");
-  }
-
-  handleValueChange = value => {
-    this.setState({ value: value });
-    console.log("handleValueChange", this.state.value);
-  };
-
-  getBoolean = () => {
-    if (this.state.value) {
-      return true;
-    }
-    return false;
-  };
-
   render() {
-    console.log("parent render", this.state.value);
     return (
       <div>
-        <div>
-          <p>parent</p>
-          <button
-            onClick={() => {
-              this.handleValueChange("parent");
-            }}
-          >
-            Click
-          </button>
-          <p>value-{this.state.value}</p>
-          <p>boolean-{this.getBoolean()}</p>
-        </div>
-        <ChildComponent
-          onValueChange={this.handleValueChange}
-          value={this.state.value}
-        />
+        <p>parent</p>
       </div>
     );
   }
